03badc825731ab8bc0b113c5a549102a65f768e7
Hyein Kim <hyeinisfree.dev@gmail.com>
2025-07-04 13:39:40 +0900

    s

---
+++ flyway/db/seed/init_terms.sql
@@ -0,0 +1,11 @@
+INSERT INTO terms (type, version, title, content, required, active, order_num, created_at, updated_at)
+VALUES ('SERVICE', 1, '서비스 이용약관', '서비스 이용에 대한 약관 내용입니다.', TRUE, TRUE, 1, NOW(), NOW());
+
+INSERT INTO terms (type, version, title, content, required, active, order_num, created_at, updated_at)
+VALUES ('SERVICE', 1, '서비스 이용약관2', '서비스 이용에 대한 약관 내용입니다2.', TRUE, TRUE, 2, NOW(), NOW());
+
+INSERT INTO terms (type, version, title, content, required, active, order_num, created_at, updated_at)
+VALUES ('PRIVACY', 1, '개인정보 처리방침', '개인정보 수집 및 이용에 대한 내용입니다.', TRUE, TRUE, 3, NOW(), NOW());
+
+INSERT INTO terms (type, version, title, content, required, active, order_num, created_at, updated_at)
+VALUES ('MARKETING', 1, '마케팅 정보 수신 동의', '이벤트 및 마케팅 정보 수신에 대한 안내입니다.', FALSE, TRUE, 4, NOW(), NOW());
